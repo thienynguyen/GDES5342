e8d010e4640890b388dbc6df5387c66dcceaef0d
thienynguyen <nguy4904@umn.edu>
2025-10-01 15:08:26 -0500

    Table java

--- MultiplicationTable/javascript.js
+++ MultiplicationTable/javascript.js
@@ -1,13 +1,27 @@
-console.log("heres a multiplication table\n");
-let timesTable = (to) => {
-    console.log("heres a multiplication table to" + to + "\n");
-    for (let i = 1; i < to + 1; i++) {
-        let row = ""
-        for (let j = 1; j < to + 1; j++) {
-            row += i*j + "\t"
+function createWithHtmlTable(rows, cols){
+    let string
+    if (rows && cols){
+        string = "<table border='1'>\n"
+        for (let row = 1; row <= rows; row++){
+            string += "<tr>\n"
+            for (let col = 1; col <= cols; col++){
+                string += "<td style=\'min-width: 40px; text-align: center\'>"
+                string += col*row + "</td>\n"
+            }
+            string += "</tr>\n"
         }
-        console.log(row)
+        string += "</table>\n"
+    } else{
+        string = "Provide some inputs."
     }
+    document.getElementById("output").innerHTML = string
 }
-timesTable(5);
-timesTable(10);
+
+function getRowsDesired(){
+    return Number(document.getElementById("rows").value);
+}
+function getColsDesired(){
+     return Number(document.getElementById("cols").value);
+}
+getRowsDesired();
+getColsDesired();
