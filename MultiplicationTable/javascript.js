641b31482dd05f2cdc15b9e66138263b92e4df3e
thienynguyen <nguy4904@umn.edu>
2025-10-01 16:49:13 -0500

    highlighting rows and columns

--- MultiplicationTable/javascript.js
+++ MultiplicationTable/javascript.js
@@ -1,11 +1,17 @@
-function createWithHtmlTable(rows, cols){
+function createWithHtmlTable(rows, cols, highlightRow, highlightCol) {
     let string
     if (rows && cols){
         string = "<table border='1'>\n"
         for (let row = 1; row <= rows; row++){
             string += "<tr>\n"
             for (let col = 1; col <= cols; col++){
-                string += "<td style=\'min-width: 40px; text-align: center\'>"
+                if (col === highlightCol){
+                    string += '<td class = "highlighted">'
+                } else if (row === highlightRow){
+                    string += '<td class = "highlighted">'
+                } else {
+                    string += "<td>"
+                }
                 string += col*row + "</td>\n"
             }
             string += "</tr>\n"
@@ -21,7 +27,11 @@ function getRowsDesired(){
     return Number(document.getElementById("rows").value);
 }
 function getColsDesired(){
-     return Number(document.getElementById("cols").value);
+    return Number(document.getElementById("cols").value);
 }
-getRowsDesired();
-getColsDesired();
+function getHighlightRow(){
+    return Number(document.getElementById("highlightRow").value);
+}
+function getHighlightCol(){
+    return Number(document.getElementById("highlightCol").value);
+}
