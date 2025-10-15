4b1920899c44b3a611dd981d0427326ca12f155d
thienynguyen <nguy4904@umn.edu>
2025-10-15 16:12:20 -0500

    fix readme placement

--- MultiplicationTable/javascript.js
+++ MultiplicationTable/javascript.js
@@ -1,40 +1,45 @@
 function createWithHtmlTable(rows, cols, highlightRow, highlightCol) {
     let string
-    if (rows && cols){
+    if (rows && cols) {
         string = "<section class = 'table'>\n"
-        for (let row = 1; row <= rows; row++){
+        for (let row = 1; row <= rows; row++) {
             string += "<div>\n"
-            for (let col = 1; col <= cols; col++){
-                if (col === highlightCol){
+            for (let col = 1; col <= cols; col++) {
+                if (col === highlightCol) {
                     string += '<aside class = "highlighted">'
-                } else if (row === highlightRow){
+                } else if (row === highlightRow) {
                     string += '<aside class = "highlighted">'
                 } else {
                     string += "<aside>"
                 }
-                string += col*row + "</aside>\n"
+                string += col * row + "</aside>\n"
             }
             string += "</div>\n"
         }
         string += "</section>\n"
-    } else{
+    } else {
         string = "Provide some inputs."
     }
     document.getElementById("output").innerHTML = string
 }
 
-function getRowsDesired(){
+function getRowsDesired() {
     return Number(document.getElementById("rows").value);
 }
-function getColsDesired(){
+
+function getColsDesired() {
     return Number(document.getElementById("cols").value);
 }
-function getHighlightRow(){
+
+function getHighlightRow() {
     return Number(document.getElementById("highlightRow").value);
 }
-function getHighlightCol(){
+
+function getHighlightCol() {
     return Number(document.getElementById("highlightCol").value);
 }
-function getInputValueAsNumber(inputId){
+
+function getInputValueAsNumber(inputId) {
     return Number(document.getElementById(inputId).value);
 }
+
